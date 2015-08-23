eac4e18f2ed5daa485e078e24229de6ce97aa008
Chloe <chloeleichen@gmail.com>
2015-08-23 16:02:31 +1000

    templating

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,27 +2,9 @@
   'use strict';
   var angular = require('angular');
   //create module
-  var beer = angular.module('beer', ['ngRoute']);
-  require('angular-router-browserify')(angular);
-
+  var beer = angular.module('beer', []);
   //create controller 
-  beer.controller('NavController', require('./controllers/NavController'));
   beer.controller('MainController', require('./controllers/MainController'));
 
-
-
-  //setup routing 
-  beer.config(['$routeProvider', function($routeProvider){
-  $routeProvider.
-    when('/', {
-      templateUrl: 'app/views/main.html',
-      controller: 'MainController'
-    }).
-    when('/about', {
-      templateUrl: 'app/views/about.html'
-    }).
-    otherwise({
-      redirectTo: '/'
-    });
-  }]);
 })();
+
