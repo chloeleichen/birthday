79d0f96cfa09d870986d4c71018d57a480839f21
Chloe Chen <chloeleichen@gmail.com>
2015-08-28 13:43:28 +1000

    protractor setup

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,6 +2,6 @@
 describe('no access', function() {
   it('stranger without the correct path will be denied access', function() {
     browser.get('index.html');
-    expect(element(by.css('title')).getText().toEqual('Hi stranger, looks like you are not on the guest list'));
+    expect(element.all(by.css(':title')).getText().toEqual('Hi stranger, looks like you are not on the guest list'));
   });
 });
